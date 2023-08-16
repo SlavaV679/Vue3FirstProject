0cfdff5efea54da02ed7b69ca39fe7de3ae8363d
SlavaV679 <77910663+SlavaV679@users.noreply.github.com>
2023-08-16 17:29:47 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="(item, idx) in items" :key="idx">
+        {{ item.name }} - {{ item.price }}
+      </li>
+    </ul>
+    <button @click="addAndUpdate">Add and update</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      items: [
+        { name: "AlphVVV", price: 1 },
+        { name: "BetaVVV", price: 2 },
+      ],
+    };
+  },
+
+  methods: {
+    addAndUpdate() {
+      const newItem = { name: "New", price: "-" };
+      this.items.push(newItem);
+      setTimeout(() => {
+        newItem.price = "over 9000";
+      }, 10);
+    },
+  },
+};
+</script>
